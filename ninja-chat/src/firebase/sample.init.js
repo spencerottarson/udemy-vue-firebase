ef31b1a6fdca37901a315e35f348d5afedf65a72
Spencer Ottarson <ottarso5@msu.edu>
2019-01-21 21:42:33 -0500

    Start chat

---
+++ ninja-chat/src/firebase/sample.init.js
@@ -0,0 +1,17 @@
+// import firebase from 'firebase'
+// import firestore from 'firebase/firestore'
+
+// // Initialize Firebase
+// var config = {
+//   apiKey: "",
+//   authDomain: "",
+//   databaseURL: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: ""
+// };
+
+// const firebaseApp = firebase.initializeApp(config);
+// firebaseApp.firestore().settings({ timestampsInSnapshots: true })
+
+// export default firebaseApp.firestore()
